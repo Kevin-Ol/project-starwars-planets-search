00179c9e06232592ae2b27a3755d72e729dd1453
Kevin Oliveira <kevin.zero@hotmail.com>
2021-09-04 19:21:54 -0300

    Updates

---
+++ precache-manifest.5c565cc6e1f92fea8e2a07302129ef8d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a674ad3be232e1481ea96fea826d5954",
+    "url": "/project-starwars-planets-search/index.html"
+  },
+  {
+    "revision": "108e574d61de61e733ad",
+    "url": "/project-starwars-planets-search/static/css/main.6d6ffef9.chunk.css"
+  },
+  {
+    "revision": "1115972b5668b46ac8a9",
+    "url": "/project-starwars-planets-search/static/js/2.7239f711.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/project-starwars-planets-search/static/js/2.7239f711.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "108e574d61de61e733ad",
+    "url": "/project-starwars-planets-search/static/js/main.4c6df014.chunk.js"
+  },
+  {
+    "revision": "f7eb459dd634f26d2408",
+    "url": "/project-starwars-planets-search/static/js/runtime-main.af1aea79.js"
+  }
+]);
